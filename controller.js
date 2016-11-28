a7046f70d8457c4742889297c5817bb3c8d517b5
Cooper189 <anton.bilousov@gmail.com>
2016-11-28 21:54:28 +0200

    new json

--- controller.js
+++ controller.js
@@ -1,7 +1,14 @@
-var app = angular.module('app', []);
+var app = angular.module('app', ['ngRoute']);
+app.config(['$routeProvider',function ($routeProvider) {
+  $routeProvider.when('/main', {
+    templateUrl: 'template/main.html'
+  });
+  $routeProvider.otherwise({ redirectTo: '/main' });
+}])
+
+
 app.filter('startFrom', function(){
   return function(input, start){
-    console.log(start);
     start = +start;
     return input.slice(start);
   }
@@ -14,14 +21,12 @@ app.directive('scc', function () {
       controllerAs: "scc",
       bindToController: true,
     controller: function (goodsService, $timeout) {
-      var self = this
+      var self = this;
       self.currentPage = 0;
-      self.itemsPerPage = 5;
-      // self.dodo = self.items.length;
+      self.itemsPerPage = 10;
       self.items = [];
        var goodsPromise = goodsService.getGoods();
       goodsPromise.then(function (val) {self.items = val;});
-       // self.dodo = self.items.length;  
       self.firstPage = function() {
     return self.currentPage == 0;
   }
@@ -32,86 +37,25 @@ app.directive('scc', function () {
   self.numberOfPages = function(){
     return Math.ceil(self.node.length / self.itemsPerPage);
   }
-  self.startingItem = function() {
-    
+  self.startingItem = function() {    
     return self.currentPage * self.itemsPerPage;
-
   }
   self.pageBack = function() {
     self.currentPage = self.currentPage - 1;
-    console.log(self.currentPage);
   }
   self.pageForward = function() {
 
     self.currentPage = self.currentPage + 1;
-console.log(self.currentPage);
 }
   self.motaro = function (x, y) {
     self.gog = y;
     self.gogo = x;
     self.currentPage = 0;
-  }
-    },
-    /*link: function (scope) {
-          scope.firstPage = function() {
-    return scope.currentPage == 0;
-  }
-  scope.lastPage = function() {
-    console.log(scope.items);
-    var lastPageNum = Math.ceil(scope.items.length / scope.itemsPerPage - 1);
-    return $scope.currentPage == lastPageNum;
-  }
-  scope.numberOfPages = function(){
-    console.log(scope.items);
-    return Math.ceil(scope.items.length / scope.itemsPerPage);
-  }
-  scope.startingItem = function() {
-    console.log(scope.items);
-    return scope.currentPage * scope.itemsPerPage;
-  }
-  scope.pageBack = function() {
-    console.log(scope.items);
-    scope.currentPage = scope.currentPage - 1;
-  }
-  scope.pageForward = function() {
-    console.log(scope.items);
-    scope.currentPage = scope.currentPage + 1;
-
-}
-    }*/
+    }
+  },
   };
 })
-/*.controller('paginationCtrl', function($scope, goodsService){
-  $scope.currentPage = 0;
-  $scope.itemsPerPage = 5;
-  var goodsPromise = goodsService.getGoods();
-      goodsPromise.then(function (val) {$scope.items = val; console.log(val)});
 
-  for(var i=0; i<25; i++){
-    $scope.items.push('Product ' + i);
-  }
-
-  $scope.firstPage = function() {
-    return $scope.currentPage == 0;
-  }
-  $scope.lastPage = function() {
-    var lastPageNum = Math.ceil($scope.items.length / $scope.itemsPerPage - 1);
-    return $scope.currentPage == lastPageNum;
-  }
-  $scope.numberOfPages = function(){
-    return Math.ceil($scope.items.length / $scope.itemsPerPage);
-  }
-  $scope.startingItem = function() {
-    return $scope.currentPage * $scope.itemsPerPage;
-  }
-  $scope.pageBack = function() {
-    $scope.currentPage = $scope.currentPage - 1;
-  }
-  $scope.pageForward = function() {
-    $scope.currentPage = $scope.currentPage + 1;
-
-}
-});*/
 app.factory('goodsService', ['$http','$q', function ($http, $q) {
   return {
     getGoods: function () {
